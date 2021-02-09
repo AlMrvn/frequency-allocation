424751ea95863f5b0bb380fd7b28ad9419ae8930
Alexis Morvan <amorvan@lbl.gov>
2021-02-09 14:30:51 -0800

    Doc

--- check_solution.py
+++ check_solution.py
@@ -4,9 +4,19 @@ import numpy as np
 import networkx as nx
 import sys
 
-# Extracting the data from a solution.txt
+# Parsing solutions from a .txt
+
+
 def extract_solution(fname: str):
-    """ Extract the solution caracteristic """
+    """ Extract the solution parameteres from a text file. The output format have to foollow a specific format 
+
+    Args:
+        fname (str): filename of the solution
+    Return:
+        freqs (np.array): numpy array with the frequencies
+        anharmonicities (np.array): numpy array with the anharmonicity
+        d (np.array): threshold used (lower bond) associated with the constraints
+    """
     with open(fname, "r") as f:
         x = f.read()
     i_freq = x.find("---- VAR f")
@@ -24,7 +34,7 @@ def extract_solution(fname: str):
     # thresholds
     d = x[i_d:].split("\n")[4:-3]
     d = np.array([float(d[k].split()[1]) for k in range(len(d))])
-    
+
     # taking care of the type 4 that don't have threshold
     dd = np.zeros(7)
     dd[[0, 1, 2, 4, 5, 6]] = d
@@ -32,150 +42,165 @@ def extract_solution(fname: str):
     return freqs, a, dd
 
 # listing of the constraints
-# we only add the unsatisfied constraint
-def type1(G: nx.DiGraph, d:float)-> bool:
+
+
+def type1(G: nx.DiGraph, d: float) -> bool:
     """ Type 1 constraint
     Args:
         G (nx.Digraph) : Directional graph of the layout
         d (float): threshold
     """
-    
+
     res = {}
-    
+
     # loop through the edges
     for i, j in G.edges:
-        b =  np.abs( G.nodes[i]['freq'] - G.nodes[j]['freq']) > d 
+        b = np.abs(G.nodes[i]['freq'] - G.nodes[j]['freq']) > d
         if not b:
-            res[(i, j)]= b
-    
+            res[(i, j)] = b
+
     return res
 
-# Now we check the graph
-def type2(G: nx.DiGraph, d:float)-> bool:
+
+def type2(G: nx.DiGraph, d: float) -> bool:
     """ Type 2 constraint
     Args:
         G (nx.Digraph) : Directional graph of the layout
         d (float): threshold
     """
-    
+
     res = {}
-    
+
     # loop through the edges
     for i, j in G.edges:
-        b = np.abs( G.nodes[i]['freq'] - G.nodes[j]['freq'] - G.nodes[j]['a']) > d 
+        b = np.abs(G.nodes[i]['freq'] - G.nodes[j]
+                   ['freq'] - G.nodes[j]['a']) > d
         if not b:
             res[(i, j)] = b
-        
-        b = np.abs( G.nodes[i]['freq'] + G.nodes[i]['a'] - G.nodes[j]['freq'] ) > d 
+
+        b = np.abs(G.nodes[i]['freq'] + G.nodes[i]
+                   ['a'] - G.nodes[j]['freq']) > d
         if not b:
-            res[(j, i)]= b
-    
+            res[(j, i)] = b
+
     return res
 
-def type3(G: nx.DiGraph, d:float)-> bool:
+
+def type3(G: nx.DiGraph, d: float) -> bool:
     """ Type 3 constraint
     Args:
         G (nx.Digraph) : Directional graph of the layout
         d (float): threshold
     """
-    
+
     res = {}
     # loop through the edges
     for i, j in G.edges:
-        b =  np.abs(G.nodes[j]['freq'] - G.nodes[i]['freq'] - G.nodes[i]['a']/2) > d 
+        b = np.abs(G.nodes[j]['freq'] - G.nodes[i]
+                   ['freq'] - G.nodes[i]['a']/2) > d
         if not b:
-            res[(i, j)]= b
+            res[(i, j)] = b
     return res
 
 
-def type4(G: nx.DiGraph, d:float)-> bool:
+def type4(G: nx.DiGraph, d: float) -> bool:
     """ Type 4 constraint
     Args:
         G (nx.Digraph) : Directional graph of the layout
         d (float): threshold
     """
-    
+
     res = {}
-    
+
     # loop through the edges
     for i, j in G.edges:
-        b1 =  G.nodes[i]['freq'] + G.nodes[i]['a'] < G.nodes[j]['freq']
-        b2 =  G.nodes[j]['freq'] < G.nodes[i]['freq']
+        b1 = G.nodes[i]['freq'] + G.nodes[i]['a'] < G.nodes[j]['freq']
+        b2 = G.nodes[j]['freq'] < G.nodes[i]['freq']
 
         b = b1 and b2
 
         if not b:
-            res[(i, j)]= b
-    
+            res[(i, j)] = b
+
     return res
 
 
-def type5(G: nx.DiGraph, d:float):
-    
+def type5(G: nx.DiGraph, d: float):
+
     res = {}
-    
+
     for (i, j) in G.edges:
-        
+
         control_neighbhors = list(nx.all_neighbors(G, i))
         control_neighbhors.remove(j)  # remnoving the target
-        
+
         for k in control_neighbhors:
             b = np.abs(G.nodes[j]['freq']-G.nodes[k]['freq']) > d
-            
+
             if not b:
                 res[(i, j, k)] = b
-    
+
     return res
 
-def type6(G: nx.DiGraph, d:float):
-    
+
+def type6(G: nx.DiGraph, d: float):
+
     res = {}
-    
+
     for (i, j) in G.edges:
-        
+
         control_neighbhors = list(nx.all_neighbors(G, i))
         control_neighbhors.remove(j)  # remnoving the target
-        
+
         for k in control_neighbhors:
-            b = np.abs(G.nodes[j]['freq']-G.nodes[k]['freq'] - G.nodes[k]['a']) > d
-            
+            b = np.abs(G.nodes[j]['freq']-G.nodes[k]
+                       ['freq'] - G.nodes[k]['a']) > d
+
             if not b:
                 res[(i, j, k)] = b
-    
+
     return res
 
-def type7(G: nx.DiGraph, d:float):
-    
+
+def type7(G: nx.DiGraph, d: float):
+
     res = {}
-    
+
     for (i, j) in G.edges:
-        
+
         control_neighbhors = list(nx.all_neighbors(G, i))
         control_neighbhors.remove(j)  # remnoving the target
-        
+
         for k in control_neighbhors:
-            b = np.abs(2*G.nodes[i]['freq'] + G.nodes[i]['a'] - G.nodes[j]['freq']-G.nodes[k]['freq'] ) > d
-            
+            b = np.abs(2*G.nodes[i]['freq'] + G.nodes[i]['a'] -
+                       G.nodes[j]['freq']-G.nodes[k]['freq']) > d
+
             if not b:
                 res[(i, j, k)] = b
-    
+
     return res
+
 
 def check(G: nx.DiGraph, thresholds: np.array):
-    
+    """
+    Check all the constraint on the graph of solution 
+    Args:
+        G (nx.DiGraph): directional graph with the frequency and anharmocity
+        thresholds (np.array): threshold associated with the differents constraints
+
+    """
+
     constraints = [type1, type2, type3, type4, type5, type6, type7]
 
     for c, d in zip(constraints, thresholds):
-        
+
         error = list(c(G, d).keys())
         if error != []:
             print(f"{c.__name__:<8} (min= {d:.03f} GHz) on {error}")
 
 
-
-
 if __name__ == '__main__':
-    
+
     fname = sys.argv[1]
 
     # extracting the data
@@ -183,7 +208,8 @@ if __name__ == '__main__':
 
     # construct the graph. here we suppose a 10 nodes graph with a specific Control-target geometry
     G = nx.DiGraph()
-    G.add_edges_from([(0, 1), (1, 2),(2, 3), (3, 4), (4, 5), (6, 5), (7, 6), (8, 7), (9, 8), (0, 9)])
+    G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4), (4, 5),
+                      (6, 5), (7, 6), (8, 7), (9, 8), (0, 9)])
     for k, n in enumerate(G.nodes):
         G.nodes[n]['freq'] = freqs[k]
         G.nodes[n]['a'] = a[k]
