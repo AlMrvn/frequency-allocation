44510e0fd62c7508b672fd58cc2cda9ee24da127
Alexis Morvan <amorvan@lbl.gov>
2021-06-17 22:35:20 -0700

    update optimization

--- freq_allocation/optimization.py
+++ freq_allocation/optimization.py
@@ -5,14 +5,9 @@ import itertools
 
 SOLVER_NAME = 'gurobi'  # cplex, glpk, gurobi
 
-# thresholds are global parameters for now
-C = {'A1': 0.017, 'A2i': 0.03, 'A2j': 0.03, 'E1': 0.017, 'E2': 0.03, 'E4': 0.002,
-     'E1t': 0.017, 'E2t': 0.03, 'E4t': 0.002, 'F1': 0.017, 'F2': 0.025, 'M1': 0.017, 'C1': 0}
-
-
-# weight of the objective function are also global parameters
-wC = {'A1': 1, 'A2i': 1, 'A2j': 1, 'E1': 1, 'E2': 1, 'E4': 1,
-      'E1t': 1, 'E2t': 1, 'E4t': 1, 'F1': 1, 'F2': 1, 'M1': 1, 'C1': 1}
+# # thresholds are global parameters for now
+# C = {'A1': 0.017, 'A2i': 0.03, 'A2j': 0.03, 'E1': 0.017, 'E2': 0.03,
+#      'E4': 0.002, 'F1': 0.017, 'F2': 0.025, 'M1': 0.017, 'C1': 0}
 
 
 class layout_optimizer():
@@ -30,6 +25,10 @@ class layout_optimizer():
         else:
             raise Exception("architecure not understood. Should be CZ or CR")
 
+        # get the weight and the constraints:
+        self.get_weight()
+        self.get_C()
+
         if qutrit:
             raise Exception("Qutrit not yet implemented properly")
 
@@ -41,7 +40,7 @@ class layout_optimizer():
 
         # initialization of variables:
         self.model.N = Set(initialize=range(0, len(graph)))
-        self.model.C = Set(initialize=C.keys())
+        self.model.C = Set(initialize=self.C.keys())
         self.model.E = Set(
             initialize=list(graph.edges))
 
@@ -60,6 +59,24 @@ class layout_optimizer():
         # declare objective function
         self.declare_objective()
 
+    def get_weight(self):
+        if self.CR_flag:
+            # weight of the objective function are also global parameters
+            self.wC = {'A1': 1, 'A2i': 1, 'A2j': 1, 'E1': 1, 'E2': 1,
+                       'E4': 1, 'F1': 1, 'F2': 1, 'M1': 1, 'C1': 1}
+        elif self.CZ_flag:
+            self.wC = {'A1': 1, 'A2i': 1, 'A2j': 1, 'E1': 1, 'E2': 1, 'E4': 1, 'E1t': 1,
+                       'E2t': 1, 'E4t': 1, 'F1': 1, 'F2': 1, 'M1': 1, 'C1': 1, 'C2': 1}
+
+    def get_C(self):
+        if self.CR_flag:
+            # weight of the objective function are also global parameters
+            self.C = {'A1': 0.017, 'A2i': 0.03, 'A2j': 0.03, 'E1': 0.017, 'E2': 0.03,
+                      'E4': 0.002, 'F1': 0.017, 'F2': 0.025, 'M1': 0.017, 'C1': 0}
+        elif self.CZ_flag:
+            self.C = {'A1': 0.017, 'A2i': 0.03, 'A2j': 0.03, 'E1': 0.017, 'E2': 0.03, 'E4': 0.002,
+                      'E1t': 0.017, 'E2t': 0.03, 'E4t': 0.002, 'F1': 0.017, 'F2': 0.025, 'M1': 0.017, 'C1': 0, 'C2': 0}
+
     def declare_decision_variables(self):
         """ Declare the variables of the models. Here we have three types of variables: 
             - the frequency of each node f
@@ -71,13 +88,13 @@ class layout_optimizer():
         m = self.model
 
         def _bounds_rule(m, c, i, j):
-            return (C[c], 100)
+            return (self.C[c], 100)
 
         # decision variables
         m.f = Var(m.N, domain=Reals, bounds=(4.52, 5.98))
         # m.f  = Var(m.N, domain=Reals, bounds=(4.5, 6))
-        # m.a  = Var(m.N, domain=Reals, bounds=(-0.35, -0.35))
-        m.a = Var(m.N, domain=Reals, bounds=(-0.35, -0.2))
+        m.a = Var(m.N, domain=Reals, bounds=(-0.3, -0.3))
+        # m.a = Var(m.N, domain=Reals, bounds=(-0.35, -0.2))
         # m.d  = Var(m.C, domain=Reals, bounds=_bounds_rule)
         m.d = Var(m.C, m.E, domain=Reals, bounds=_bounds_rule)
         m.fd = Var(m.E, domain=Reals, bounds=(4.52, 5.98))
@@ -161,22 +178,22 @@ class layout_optimizer():
                 m.c.add(m.fd[i, j] - m.f[j] - m.a[j] >= m.d['C2', (i, j)])
                 m.c.add(m.f[j] - m.fd[i, j] >= m.d['C2', (i, j)])
 
-            for (i, j, k) in self.Neigh:
-                m.c.add(m.fd[i, j] - m.f[k] + big_M *
-                        m.n1[i, j, k] >= m.d['F1', (i, j)])
-                m.c.add(-m.fd[i, j] + m.f[k] + big_M *
-                        (1-m.n1[i, j, k]) >= m.d['F1', (i, j)])
-                m.c.add(m.fd[i, j] - m.f[k] - m.a[k] + big_M *
-                        m.n2[i, j, k] >= m.d['F2', (i, j)])
-                m.c.add(-m.fd[i, j] + m.f[k] + m.a[k] + big_M *
-                        (1-m.n2[i, j, k]) >= m.d['F2', (i, j)])
-                # m.c.add(  m.fd[i,j] - m.f[k] - 2*m.a[k] + big_M*m.n3[i,j,k]     >= m.d['F3',(i,j)])
-                # m.c.add( -m.fd[i,j] + m.f[k] + 2*m.a[k] + big_M*(1-m.n3[i,j,k]) >= m.d['F3',(i,j)])
+        for (i, j, k) in self.Neigh:
+            m.c.add(m.fd[i, j] - m.f[k] + big_M *
+                    m.n1[i, j, k] >= m.d['F1', (i, j)])
+            m.c.add(-m.fd[i, j] + m.f[k] + big_M *
+                    (1-m.n1[i, j, k]) >= m.d['F1', (i, j)])
+            m.c.add(m.fd[i, j] - m.f[k] - m.a[k] + big_M *
+                    m.n2[i, j, k] >= m.d['F2', (i, j)])
+            m.c.add(-m.fd[i, j] + m.f[k] + m.a[k] + big_M *
+                    (1-m.n2[i, j, k]) >= m.d['F2', (i, j)])
+            # m.c.add(  m.fd[i,j] - m.f[k] - 2*m.a[k] + big_M*m.n3[i,j,k]     >= m.d['F3',(i,j)])
+            # m.c.add( -m.fd[i,j] + m.f[k] + 2*m.a[k] + big_M*(1-m.n3[i,j,k]) >= m.d['F3',(i,j)])
 
-                m.c1.add(m.fd[i, j] + m.f[k] - 2*m.f[i] - m.a[i] +
-                         big_M*m.m1[i, j, k] >= m.d['M1', (i, j)])
-                m.c1.add(-m.fd[i, j] - m.f[k] + 2*m.f[i] + m.a[i] +
-                         big_M*(1-m.m1[i, j, k]) >= m.d['M1', (i, j)])
+            m.c1.add(m.fd[i, j] + m.f[k] - 2*m.f[i] - m.a[i] +
+                     big_M*m.m1[i, j, k] >= m.d['M1', (i, j)])
+            m.c1.add(-m.fd[i, j] - m.f[k] + 2*m.f[i] + m.a[i] +
+                     big_M*(1-m.m1[i, j, k]) >= m.d['M1', (i, j)])
 
     def all_frequency_differents(self):
 
@@ -196,7 +213,7 @@ class layout_optimizer():
     def declare_objective(self):
         """Declare objective function"""
         # m.obj = Objective(expr=sum(m.d[c,e]-C[c] for c in m.C for e in m.E), sense=maximize)
-        self.model.obj = Objective(expr=sum(wC[c]*(self.model.d[c, e]-C[c])
+        self.model.obj = Objective(expr=sum(self.wC[c]*(self.model.d[c, e]-self.C[c])
                                             for c in self.model.C for e in self.model.E), sense=maximize)
 
     def declare_solver(self):
@@ -215,11 +232,11 @@ class layout_optimizer():
         m = self.model
         m.tmp_cons = ConstraintList()
         m.tmp_linking_cons = ConstraintList()
-        B = [key for key in C.keys()]
+        B = [key for key in self.C.keys()]
         for k, c in enumerate(B):
             if k != len(B)-1:
                 m.tmp_linking_cons.add(m.d[B[k], m.E[1]] -
-                                       C[B[k]] == m.d[B[k+1], m.E[1]] - C[B[k+1]])
+                                       self.C[B[k]] == m.d[B[k+1], m.E[1]] - self.C[B[k+1]])
             for e in m.E:
                 m.tmp_cons.add(m.d[c, m.E[1]] == m.d[c, e])
         results = self.solver.solve(m)
@@ -231,10 +248,11 @@ class layout_optimizer():
         """
         m = self.model
         m.tmp_cons_2 = ConstraintList()
-
-        best_value_with_deltas_same = value(m.d[m.C[1], m.E[1]]) - C[m.C[1]]
-        for c in C:
-            m.tmp_cons_2.add(m.d[c, m.E[1]] - C[c] >=
+        m.tmp_linking_cons.clear()
+        best_value_with_deltas_same = value(
+            m.d[m.C[1], m.E[1]]) - self.C[m.C[1]]
+        for c in self.C:
+            m.tmp_cons_2.add(m.d[c, m.E[1]] - self.C[c] >=
                              best_value_with_deltas_same)
 
         results = self.solver.solve(m)
