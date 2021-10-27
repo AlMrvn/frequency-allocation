36f7b1d86639feb56a85363fb7bbf72afaa15b2e
Alexis Morvan <amorvan@lbl.gov>
2021-10-26 21:08:04 -0700

    script for yield

--- scripts/calculate_yield_CZ.py
+++ scripts/calculate_yield_CZ.py
@@ -1,11 +1,12 @@
-
-from freq_allocation.yield_mc_simulation import *
-from freq_allocation.frequency_graph import FrequencyGraph
-import logging
-import numpy as np
-
 import sys
 sys.path.append("../")
+
+import numpy as np
+import logging
+from freq_allocation.frequency_graph import FrequencyGraph
+from freq_allocation.yield_mc_simulation import *
+
+
 
 # silencing pyomo warning
 logging.getLogger('pyomo.core').setLevel(logging.ERROR)
@@ -13,55 +14,70 @@ logging.getLogger('pyomo.core').setLevel(logging.ERROR)
 
 if __name__ == '__main__':
 
-    fname = sys.argv[1]
+    fname_list = [
+        "heavy_hexagon_CZ_3MHz.npz",
+        "hexagon_CZ_3MHz.npz",
+        "square_CZ_3MHz.npz",
+        "linear_CZ_3MHz.npz",
+        "heavy_hexagon_CZ_5MHz.npz",
+        "hexagon_CZ_5MHz.npz",
+        "square_CZ_5MHz.npz",
+        "linear_CZ_5MHz.npz",
+        "heavy_hexagon_CZ_10MHz.npz",
+        "hexagon_CZ_10MHz.npz",
+        "square_CZ_10MHz.npz",
+        "linear_CZ_10MHz.npz",
+    ]
 
-    # yield parameters
-    Nsamples = 2000  # sample with re-optimization
-    s_vec = np.linspace(0, 0.040, 16)
+    for fname in fname_list:
 
-    # Loading the graph
-    print("Loading the circuit solution")
+        # yield parameters
+        Nsamples = 2000  # sample with re-optimization
+        s_vec = np.linspace(0, 0.040, 16)
 
-    with np.load(fname, allow_pickle=True) as file:
-        edges = file["edges"]
-        freqs = file["freqs"][()]
-        anharms = file["anharms"][()]
-        drives = file["drives"][()]
-        architecture = file["architecture"]
-        qutrit = file["qutrit"]
-        d_dict = file["constraints"][()]
-        qutrit = file["qutrit"]
+        # Loading the graph
+        print("Loading the circuit solution")
 
-    G = FrequencyGraph(edges=edges, architecture=architecture)
-    G.set_values(freqs, anharms, drives)
+        with np.load(fname, allow_pickle=True) as file:
+            edges = file["edges"]
+            freqs = file["freqs"][()]
+            anharms = file["anharms"][()]
+            drives = file["drives"][()]
+            architecture = file["architecture"]
+            qutrit = file["qutrit"]
+            d_dict = file["constraints"][()]
+            qutrit = file["qutrit"]
 
-    # Calculating the yield
-    data = []
-    # varying the dispersion of the frequency
+        G = FrequencyGraph(edges=edges, architecture=architecture)
+        G.set_values(freqs, anharms, drives)
 
-    n_collisions = [0]
+        # Calculating the yield
+        data = []
+        # varying the dispersion of the frequency
 
-    # saving the results
-    for k, s in enumerate(s_vec):
-        print(f"Currently at {k/len(s_vec)*100:0.0f} %")
+        # n_collisions = [0]
 
-        # getting the collisisons
-        collisions = G.get_collision(d_dict,
-                                     Nsamples=Nsamples,
-                                     sigma=s,
-                                     cstr=list(d_dict.keys()),
-                                     reoptimize=True)[0]
+        # # saving the results
+        # for k, s in enumerate(s_vec):
+        #     print(f"Currently at {k/len(s_vec)*100:0.0f} %")
 
-        # looking at the number of collisions
-        y = [(Nsamples-np.count_nonzero(collisions-n)) /
-             Nsamples for n in n_collisions]
+        #     # getting the collisisons
+        #     collisions = G.get_collision(d_dict,
+        #                                  Nsamples=Nsamples,
+        #                                  sigma=s,
+        #                                  cstr=list(d_dict.keys()),
+        #                                  reoptimize=True)[0]
 
-        # we only keep the zero collision data
-        data.append(y[0])
-        print(f"yield: {y[0]*100:0.0f} %")
+        #     # looking at the number of collisions
+        #     y = [(Nsamples-np.count_nonzero(collisions-n)) /
+        #          Nsamples for n in n_collisions]
 
-        # let finish when the yield is way too low
-        if y[0] < 1.0e-1:
-            break
+        #     # we only keep the zero collision data
+        #     data.append(y[0])
+        #     print(f"yield: {y[0]*100:0.0f} %")
 
-        np.save(fname.split(".")[0] + "yield.npz", data)
+        #     # let finish when the yield is way too low
+        #     if y[0] < 1.0e-1:
+        #         break
+
+        np.save(fname.split(".")[0] + "yield", data)
